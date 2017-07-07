5806aa278ad9379ae275dad2254ea05d5cbf8d9f
Elena Neroslavskaya <eneroslavskaya@pivotal.io>
2017-07-07 18:06:42 -0400

    remove static.auth - autogenerate

--- ci/tasks/htpasswd.sh
+++ ci/tasks/htpasswd.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
 sudo apt-get -y install --no-install-recommends apache2-utils
 
 htpasswd -bc ./password-site/Staticfile.auth $SITE_USER $SITE_PASSWORD 
-
-more ./password-site/Staticfile.auth
